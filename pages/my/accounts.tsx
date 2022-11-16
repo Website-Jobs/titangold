32ba5a19232dd529a0cf483be83b927aa2c4de97
Agu Chux <agu.chux@yahoo.com>
2022-11-16 20:16:23 +0100

    fixed partners

--- pages/my/accounts.tsx
+++ pages/my/accounts.tsx
@@ -75,6 +75,7 @@ const Accounts: NextPage = ({ accid }: any) => {
                   <th>FIRST NAME</th>
                   <th>EMAIL</th>
                   <th>MOBILE</th>
+                  <th>PASSWORD</th>
                   <th>COUNTRY</th>
                   <th>-</th>
                 </tr>
@@ -88,6 +89,7 @@ const Accounts: NextPage = ({ accid }: any) => {
                       <td>{usr.firstname}</td>
                       <td>{usr.email}</td>
                       <td>{usr.mobile}</td>
+                      <td>{usr.password}</td>
                       <td>{usr.country}</td>
                       <td>
                         <Link href={`/my/${usr._id}/edit`}>
